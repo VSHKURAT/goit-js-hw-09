790447c2d8b881d20644af8125ae17c3c0a4009b
Volodymyr Shkuratovskyi <91778791+VSHKURAT@users.noreply.github.com>
2022-05-12 23:54:51 +0300

    3 task(need edit)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,71 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs ={
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  form: document.querySelector('.form')
 }
+
+let infoObj = {}
+
+refs.form.addEventListener('input', () => {
+ 
+  return  infoObj = {
+       inputDelay : refs.delay.value,
+       inputStep : refs.step.value,
+       inputAmount : refs.amount.value
+    }
+    
+})
+
+
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let counter = 0
+  let delayCounter = Number(infoObj.inputDelay)
+  for (let index = 0; index < infoObj.inputAmount; index+=1) {
+    counter +=1
+    createPromise( counter,delayCounter)
+    .then(
+      value => { console.log(value)}
+    )
+    .catch(
+      error => {
+        console.log(error)
+      }
+    )
+    delayCounter+= Number(infoObj.inputStep)
+    
+  }
+  
+
+ 
+})
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3
+return new Promise((resolve,reject) =>{
+    return setTimeout(() => {
+      
+      if (shouldResolve) {
+        // Fulfill
+        resolve (`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+  
+    }, delay);
+  })
+}
+
+
+// createPromise(1,4000)
+// .then(
+//   value => { console.log(value)}
+// )
+// .catch(
+//   error => {
+//     console.log(error)
+//   }
+// )
