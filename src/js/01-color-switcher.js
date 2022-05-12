568b73b2c5dbd27215da062bf3c87a1d4eaac9a3
Volodymyr Shkuratovskyi <91778791+VSHKURAT@users.noreply.github.com>
2022-05-12 16:26:44 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const refs= {
+    background : document.querySelector('body'),
+    startBtn : document.querySelector(`button[data-start`),
+    stopBtn : document.querySelector(`button[data-stop]`)
+}
+let intervalId = null
+
+console.log(refs.startBtn)
+console.log(refs.stopBtn)
+
+refs.startBtn.addEventListener ('click' , runColorChange)
+refs.stopBtn.addEventListener('click' , stopColorChange)
+
+function runColorChange(){
+  refs.startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(()=>{
+        refs.background.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    
+}
+
+function stopColorChange(){
+
+clearInterval(intervalId)
+refs.startBtn.removeAttribute('disabled', true)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
